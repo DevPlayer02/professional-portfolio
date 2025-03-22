c2d44a471ef0d5ed64b4b677a3bd7cd44ff4b718
DevPlayer02 <contato.giovanicf@gmail.com>
2025-03-22 19:26:15 -0300

    Feat: Adding mock projects

--- src/pages/ServicesProvided.tsx
+++ src/pages/ServicesProvided.tsx
@@ -1,9 +1,17 @@
-function ServicesProvided() {
+import { Container, Grid } from "@mui/material";
+import { ServiceCards, StyledH1 } from "../components";
+
+function Projects() {
   return (
-    <>
-      <div>ServicesProvided</div>
-    </>
+    <Container>
+      <StyledH1 className="mb-2">Veja um pouco do meu trabalho abaixo...</StyledH1>
+      <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <ServiceCards /> 
+          </Grid>
+      </Grid>
+    </Container>
   );
 }
 
-export default ServicesProvided;
+export default Projects;
